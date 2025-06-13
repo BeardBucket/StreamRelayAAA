3f8f73739e39fa4550df66c913132d03100f7187
Paulson McIntyre <paul@gpmidi.net>
2025-06-12 21:55:52 -0400

    Added models and admin site deets for stream basics

--- relay/streamaaa/admin.py
+++ relay/streamaaa/admin.py
@@ -1,3 +1,148 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.admin import GroupAdmin as AuthGroupAdmin
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = StreamUser
+    max_num = 1
+    can_delete = False
+    can_view_details = True
+    can_edit = False
+    readonly_fields = ('guid', 'quick')
+    fieldsets = (
+        ("Details", {'fields': ("handle", 'guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+
+
+class StreamKeyInline(admin.StackedInline):
+    model = StreamKey
+    can_view_details = True
+    can_edit = False
+    readonly_fields = ('guid', 'quick','user',)
+    fieldsets = (
+        ("Details", {'fields': ("enabled","key",)}),
+        ("Extra", {'fields': ('guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+
+class UserAdmin(AuthUserAdmin):
+    inlines = [
+        UserProfileInline,
+        StreamKeyInline,
+    ]
+    list_display = ["handle", "username", "first_name", "last_name", "email"]
+
+    def handle(self, obj):
+        return obj.streamuser.handle
+
+# unregister old user admin
+admin.site.unregister(User)
+# register new user admin
+admin.site.register(User, UserAdmin)
+
+
+class GroupProfileInline(admin.StackedInline):
+    model = StreamGroup
+    max_num = 1
+    can_delete = False
+    can_edit = False
+    can_view_details = True
+    readonly_fields = ('guid', 'quick')
+    fieldsets = (
+        ("Details", {'fields': ("handle", 'guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+
+
+class GroupAdmin(AuthGroupAdmin):
+    inlines = [GroupProfileInline]
+
+
+# unregister old user admin
+admin.site.unregister(Group)
+# register new user admin
+admin.site.register(Group, GroupAdmin)
+
+
+class RelayPullInline(admin.StackedInline):
+    model = GroupRelayAccess.relays_pull.through
+    can_view_details = True
+    empty_value_display = "-empty-"
+    verbose_name = "Pull Relay Access"
+    verbose_name_plural = "Pull Relay Access"
+
+
+class RelayPushInline(admin.StackedInline):
+    model = GroupRelayAccess.relays_push.through
+    can_view_details = True
+    empty_value_display = "-empty-"
+    verbose_name = "Push Relay Access"
+    verbose_name_plural = "Push Relay Access"
+
+
+@admin.register(GroupRelayAccess)
+class GroupRelayAccessAdmin(admin.ModelAdmin):
+    readonly_fields = ('guid', 'quick')
+    fieldsets = (
+        ('Details', {'fields': ('group',)}),
+        ("Extra", {'fields': ('guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+    list_display = ["group", "quick"]
+    inlines = [
+        RelayPushInline,
+        RelayPullInline,
+    ]
+    exclude = (
+        'relay_pull',
+        'relay_push',
+    )
+
+
+class GroupRelayAccessPushInline(admin.StackedInline):
+    model = Relay.with_push.through
+    verbose_name = "Push Relay Access"
+    verbose_name_plural = "Push Relay Access"
+    # readonly_fields = ('guid', 'quick')
+    fieldsets = (
+        # ('Details', {'fields': ('group',)}),
+        # ("Extra", {'fields': ('guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+    list_display = ["group", "quick"]
+    exclude = (
+        'relay_pull',
+        'relay_push',
+    )
+    can_view_details = True
+
+
+class GroupRelayAccessPullInline(admin.StackedInline):
+    model = Relay.with_pull.through
+    verbose_name = "Pull Relay Access"
+    verbose_name_plural = "Pull Relay Access"
+    empty_value_display = "-empty-"
+    list_display = ["group", "quick"]
+    exclude = (
+        'relay_pull',
+        'relay_push',
+    )
+    can_view_details = True
+
+@admin.register(Relay)
+class RelayAdmin(admin.ModelAdmin):
+    readonly_fields = ('guid', 'quick')
+    fieldsets = (
+        ("Details", {'fields': ('name',)}),
+        ("Extra", {'fields': ('guid', 'quick')}),
+    )
+    empty_value_display = "-empty-"
+    list_display = ["name", "quick"]
+    inlines = [
+        GroupRelayAccessPushInline,
+        GroupRelayAccessPullInline,
+    ]
--- relay/streamaaa/models.py
+++ relay/streamaaa/models.py
@@ -2,41 +2,58 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User, Group
+from names_generator import generate_name
+
 
 class StreamUser(models.Model):
     """ Extra fields off User model """
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,null=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False,null=False)
-    quick = models.SlugField(max_length=100, unique=True, editable=False,null=False)
+    quick = models.SlugField(max_length=100,default=generate_name, unique=True, editable=False,null=False)
+    handle = models.CharField(max_length=100,unique=True, null=False, editable=True, blank=False)
+
+    def __str__(self):
+        return f"{self.user.username} | {self.handle}"
 
 
 class StreamGroup(models.Model):
     """ Extra fields off Group model """
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,null=False)
     group = models.OneToOneField(Group, on_delete=models.CASCADE, editable=False,null=False)
-    quick = models.SlugField(max_length=100, unique=True, editable=False,null=False)
+    quick = models.SlugField(max_length=100, default=generate_name,unique=True, editable=False,null=False)
+    handle = models.CharField(max_length=100,unique=True, null=False, editable=True, blank=False)
 
+    def __str__(self):
+        return f"{self.group.name} | {self.handle}"
 
 class StreamKey(models.Model):
     """ Stream keys """
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,null=False)
-    quick = models.SlugField(max_length=100, unique=True, editable=False,null=False)
+    quick = models.SlugField(max_length=100,default=generate_name,unique=True, editable=False,null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False,null=False)
+    key = models.SlugField(max_length=128,default=generate_name,unique=True, editable=True,null=False)
+    enabled = models.BooleanField(default=False, editable=True, null=False,blank=False)
 
+    def __str__(self):
+        return f"Key-{self.key}"
 
 class Relay(models.Model):
     """ A stream relay server """
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,null=False)
-    quick = models.SlugField(max_length=100, unique=True, editable=False,null=False)
+    quick = models.SlugField(max_length=100,default=generate_name, unique=True, editable=False,null=False)
     name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f"Relay-{self.name}"
 
 
 class GroupRelayAccess(models.Model):
     """ Relay access to Group model """
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,null=False)
-    quick = models.SlugField(max_length=100, unique=True, editable=False,null=False)
+    quick = models.SlugField(max_length=100,default=generate_name, unique=True, editable=False,null=False)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     relays_push = models.ManyToManyField(Relay, related_name='with_push')
     relays_pull = models.ManyToManyField(Relay, related_name='with_pull')
 
-
+    def __str__(self):
+        return f"RelayAccess-{self.group.name}"
