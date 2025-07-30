9ec3a2556449c97fe9ed6f961eac39e0ea4729b1
Paulson McIntyre <paul@gpmidi.net>
2025-07-30 13:22:52 -0400

    Fixing generate_name_f so that it returns a func

--- relay/streamaaa/names.py
+++ relay/streamaaa/names.py
@@ -1,12 +1,17 @@
 import os
+import functools
 from .words import ALL,NAMES,WORDS
 
-def generate_name_f(num: int = 4) -> str:
+def generate_name_f(num: int = 4):
     """ Generate a random set of words with a count of `count` words"""
-    total = len(ALL)
-    words = []
-    for i in range(num):
-        iv=int.from_bytes(os.urandom(16), byteorder="big")
-        word=ALL[iv%total]
-        words.append(word.capitalize())
-    return "".join(words)
+    @functools.wraps(generate_name_f)
+    def wrapped():
+        total = len(ALL)
+        words = []
+        for i in range(num):
+            iv=int.from_bytes(os.urandom(16), byteorder="big")
+            word=ALL[iv%total]
+            words.append(word.capitalize())
+        return "".join(words)
+
+    return wrapped
